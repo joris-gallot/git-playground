e06d0117b10e0df302bbd70c6b451258442ddb35
Joris Gallot <joris.gallot18@gmail.com>
2026-03-31 19:00:31 +0200

    chore: add get last notif

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -1,1 +1,7 @@
-export class NotificationService { send(msg: string) { console.log(msg); } }
+export class NotificationService {
+  send(msg: string) {
+    console.log(msg);
+  }
+}
+
+const getLastNotification = () => "You have a new message!";
